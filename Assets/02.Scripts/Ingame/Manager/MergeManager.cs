40a52a886cb27cbd5ccabc195e62bb56b11d5b35
정종혁 <whdgur0068@gmail.com>
2026-01-26 18:19:05 +0900

    refactor : 코드 리뷰 리팩토링

--- Assets/02.Scripts/Ingame/Manager/MergeManager.cs
+++ Assets/02.Scripts/Ingame/Manager/MergeManager.cs
@@ -18,9 +18,9 @@ public class MergeManager : MonoBehaviour
         }
     }
 
-    public bool CanMerge(ClickTarget target_1, ClickTarget target_2)
+    public bool CanMerge(ClickTarget target1, ClickTarget target2)
     {
-        return target_1 != null && target_2 != null && target_1 != target_2 && target_1.Level == target_2.Level && target_1.Level < _maxLevel;
+        return target1 != null && target2 != null && target1 != target2 && target1.Level == target2.Level && target1.Level < _maxLevel;
     }
 
     public void Merge(ClickTarget keeper, ClickTarget removed)
