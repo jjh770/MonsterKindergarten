775fd9a5b0f295d68fde18a0b12e441e52507cc2
정종혁 <whdgur0068@gmail.com>
2026-01-27 15:39:18 +0900

    feat : Sprite outline 에셋 추가 + 슬라임에 외곽선 추가

--- Assets/02.Scripts/Ingame/Floater/PointFloater.cs
+++ Assets/02.Scripts/Ingame/Floater/PointFloater.cs
@@ -9,6 +9,8 @@ public class PointFloater : MonoBehaviour
     [SerializeField] private TextMeshPro _text;
     [SerializeField] private float _floatDistance = 1f;
     [SerializeField] private float _duration = 0.8f;
+    [SerializeField] private Ease _floatEase;
+    [SerializeField] private Ease _fadeEase;
 
     private LeanGameObjectPool _pool;
     private int _levelIndex;
@@ -24,8 +26,8 @@ public class PointFloater : MonoBehaviour
         _text.alpha = 1f;
 
         // 위로 떠오르면서 페이드아웃
-        transform.DOMoveY(position.y + _floatDistance, _duration).SetEase(Ease.OutCubic);
-        _text.DOFade(0f, _duration).SetEase(Ease.InQuad).OnComplete(() =>
+        transform.DOMoveY(position.y + _floatDistance, _duration).SetEase(_floatEase);
+        _text.DOFade(0f, _duration).SetEase(_fadeEase).OnComplete(() =>
         {
             _pool.Despawn(gameObject);
         });
