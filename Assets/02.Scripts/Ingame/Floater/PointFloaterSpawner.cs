fba458109883ed4cf0a411f25b2910aebfb57b08
jjh770 <whdgur0068@gmail.com>
2026-09-19 16:21:59 +0900

    Fix : 포인트 플로팅 텍스트가 상단 바에 가려지는 문제 수정
    - 플로팅 텍스트를 HUD 위 전용 오버레이 캔버스의 UI 글자로 전환
    - 움직임은 월드 좌표로 계산하고 매 프레임 화면 위치로 투영
    - 팝업과 튜토리얼 대화보다는 아래에 그려지도록 정렬 순서 1로 배치

--- Assets/02.Scripts/Ingame/Floater/PointFloaterSpawner.cs
+++ Assets/02.Scripts/Ingame/Floater/PointFloaterSpawner.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 public class PointFloaterSpawner : MonoBehaviour
 {
     public static PointFloaterSpawner Instance { get; private set; }
+
+    [Tooltip("숫자를 그릴 오버레이 캔버스입니다. HUD보다 위, 팝업보다 아래에 둡니다.")]
+    [SerializeField] private RectTransform _floaterRoot;
+
     private LeanGameObjectPool _pool;
 
     private void Awake()
@@ -22,7 +26,7 @@ public class PointFloaterSpawner : MonoBehaviour
     public void ShowFloater(ClickInfo clickInfo)
     {
         // 1. 풀로부터 Floater 를 가져오고
-        GameObject floaterObject = _pool.Spawn(clickInfo.Position, Quaternion.identity);
+        GameObject floaterObject = _pool.Spawn(_floaterRoot, false);
         PointFloater floater = floaterObject.GetComponent<PointFloater>();
         // 2. 풀 참조 설정
         floater.SetPool(_pool);
